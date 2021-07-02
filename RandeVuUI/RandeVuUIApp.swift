39c597b460b12cef2b0f37ec58649ed31f83fddb
Chika Ohaya <chika.ohaya@gmail.com>
2021-07-01 23:28:24 -0400

    Initial Commit

---
+++ RandeVuUI/RandeVuUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandeVuUIApp.swift
+//  RandeVuUI
+//
+//  Created by Chika Ohaya on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct RandeVuUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
